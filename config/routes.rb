765be6d91357eeeea7f2f3333ce62bb333002e9c
Mimimi <mirana2>
2018-09-03 17:13:17 +0300

    lalalala

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+  root 'home#index'
+=======
   get 'session/new'
+<<<<<<< HEAD
   root 'home#index'
   get '/about', to: "home#about", as: "about"
   get '/course', to: "home#course"
+=======
+
+>>>>>>> fdf138e40ec49443306c2326cd9aac0775bb1c19
+>>>>>>> b19105e3318b8652a8b4508a2cee656001828735
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
